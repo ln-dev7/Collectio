85e4a566ab3aa0bdff273d0e20083db2e1cede06
ln-dev7 <leonelngoya@gmail.com>
2022-04-01 03:36:05 +0100

    ajout des images

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,12 @@
 window.addEventListener('load', () => {
     const count = document.querySelector('.loader-count')
-    // Incremente le count jusqu'a 100 pendant 10 secondes
     const interval = setInterval(() => {
         count.textContent = parseInt(count.textContent) + 1
         if (parseInt(count.textContent) >= 100) {
             clearInterval(interval)
             document.querySelector('.loader').classList.add('loader-end');
         }
-    }, 10)
+    }, .1)
 })
 
 
