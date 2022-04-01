9576415c318a68589296662d7160099f763d74ee
ln-dev7 <leonelngoya@gmail.com>
2022-04-01 03:10:19 +0100

    ajout du loader

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,31 @@
-// fichier js de base
+window.addEventListener('load', () => {
+    const count = document.querySelector('.loader-count')
+    // Incremente le count jusqu'a 100 pendant 10 secondes
+    const interval = setInterval(() => {
+        count.textContent = parseInt(count.textContent) + 1
+        if (parseInt(count.textContent) >= 100) {
+            clearInterval(interval)
+            document.querySelector('.loader').classList.add('loader-end');
+        }
+    }, 10)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
